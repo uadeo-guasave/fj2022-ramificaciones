55cfc95c9fe3ae1fbdb45d2662e815d1e76500d4
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-02-24 11:13:47 -0700

    Conexión a MySQL
    - Agregar clase para conectar a MySQL

---
+++ app/db/mysql.php
@@ -0,0 +1,12 @@
+<?php
+class MysqlConnection {
+    public $host = '127.0.0.1';
+    public $user = 'root';
+    public $passwd = '';
+    public $dbname = 'gitdb';
+    public $port = 3306;
+
+    public function __constructor() {
+        // TODO: abrir conexión
+    }
+}
